881ebb8bfcfe9ac0e1954cefceec7596746607b1
pritamlondhe <120086443+pritamlondhe@users.noreply.github.com>
2023-01-22 11:46:27 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function valueBtn(a){
+
+    form.screen.value+=a.value;
+
+}
+
+function Clear(){
+
+    form.screen.value=null;
+
+}
+
+function equals(){
+
+    form.screen.value=eval(form.screen.value);
+
+}
